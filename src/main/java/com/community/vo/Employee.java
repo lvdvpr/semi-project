8cacfb652870ed8121dbb78b05ac0df31edfe381
Hyunjae Seo <drizzledew@naver.com>
2022-12-08 12:18:59 +0900

    로그인/로그아웃/헤더(로그인 하면 내 정보보기 보이도록 구현)

---
+++ src/main/java/com/community/vo/Employee.java
@@ -0,0 +1,119 @@
+package com.community.vo;
+
+import java.util.Date;
+
+public class Employee {
+
+	private int no;
+	private String password;
+	private String name;
+	private String email;
+	private String phone;
+	private int deptNo;
+	private int positionNo;
+	private String type;
+	private String deleted;
+	private Date createdDate;
+	private Date updatedDate;
+	private Date deletedDate;
+	
+	Employee () {}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getDeptNo() {
+		return deptNo;
+	}
+
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+
+	public int getPositionNo() {
+		return positionNo;
+	}
+
+	public void setPositionNo(int positionNo) {
+		this.positionNo = positionNo;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(String deleted) {
+		this.deleted = deleted;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	public Date getDeletedDate() {
+		return deletedDate;
+	}
+
+	public void setDeletedDate(Date deletedDate) {
+		this.deletedDate = deletedDate;
+	}
+	
+	
+}
