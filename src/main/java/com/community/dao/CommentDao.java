fec0b6e1e49b1583c6a60fe2810a405212e62e3d
psh <psh19709@gmail.com>
2022-12-13 15:28:53 +0900

    Revert "커밋하지 않은 내용 추가(my comment, my notice)"
    This reverts commit e241a40b6b862015fa33ab879f7b0378dc6231ee.

--- src/main/java/com/community/dao/CommentDao.java
+++ src/main/java/com/community/dao/CommentDao.java
@@ -1,7 +1,6 @@
 package com.community.dao;
 
 import java.util.List;
-import java.util.Map;
 
 import com.community.util.SqlMapper;
 import com.community.vo.Comment;
@@ -10,18 +9,13 @@ public class CommentDao {
 	
 	// 작성자 직원번호를 전달 받아서 해당 직원 번호의 사람이 작성한 댓글 리스트를 반환한다.
 	@SuppressWarnings("unchecked")
-	public List<Comment> getComments(Map<String, Object> param) {
-		return (List<Comment>)SqlMapper.selectList("mypages.getComments", param);
-	}
-	
-	// 댓글 작성자, 직원번호를 전달 받아서 해당 직원번호의 사람이 작성한 댓글을 반환한다.
-	public int getTotalCommentRows(Map<String, Object> param) {
-		return (Integer)SqlMapper.selectOne("mypages.getTotalCommentRows", param);
+	public List<Comment> getCommentsByCommentNo(int commentNo) {
+		return (List<Comment>)SqlMapper.selectList("mypages.getCommentsByCommentNo", commentNo);
 	}
 	
 	// 작성자 직원번호를 전달 받아서 해당 직원 번호의 사람이 작성한 댓글 갯수를 반환한다.
-	public int getTotalCommentRowsByEmpNo(int empNo) {
-		return(Integer)SqlMapper.selectOne("mypages.getTotalCommentRowsByEmpNo", empNo);
+	public int getTotalCommentRows(int empNo) {
+		return(Integer)SqlMapper.selectOne("mypages.getTotalCommentRows", empNo);
 	}
 
 }
